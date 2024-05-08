b89fd0440aed0dbff057216f4fa36f62ca23d5c7
Felipe Carvalho <132091880+FelipeCarvalhoSilva@users.noreply.github.com>
2024-05-08 15:15:43 -0300

    LIP

---
+++ FACULDADE/Lip2023-FelipeCarvalho-main/script.js
@@ -0,0 +1,16 @@
+bootstrap.bundle.js
+bootstrap.bundle.min.js
+const jogarBtn = document.getElementById("jogar-btn");
+const divJogo = document.getElementById("div-jogo");
+  $(document).ready(function(){
+    $('.carousel').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    });
+  });
+
+
+
